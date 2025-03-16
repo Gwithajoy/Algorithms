c951a2d4d8a6f42a3817c46337889beb7f1b5b97
Gwithjoy <bangbabo131@gmail.com>
2025-03-17 00:25:24 +0900

    [Sorting] 기본 정렬 알고리즘 구현 및 테스트 코드 추가
    - 버블 정렬, 삽입 정렬, 선택 정렬 기능 구현
    - main()에서 각 알고리즘의 테스트 코드 추가

--- src/Sorting/BasicSorting/BasicSortingAlgorithms.java
+++ src/Sorting/BasicSorting/BasicSortingAlgorithms.java
@@ -7,17 +7,59 @@ public class BasicSortingAlgorithms {
 
     // 버블 정렬
     public static void bubbleSort(int[] arr) {
-
+        for (int i = 1; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j + 1] = tmp;
+                }
+            }
+            
+        }
     }
     
     // 삽입 정렬
     public static void insertionSort(int[] arr) {
-
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j-1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     // 선택 정렬
     private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length ; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
 
+//        for (int i = arr.length; i > 0; i++) {
+//            int max = i;
+//            for (int j = i - 1; j >= 0; j--) {
+//                if (arr[j] > arr[max]) {
+//                    max = j;
+//                }
+//            }
+//            int tmp = arr[i];
+//            arr[i] = arr[max];
+//            arr[max] = tmp;
+//
+//        }
     }
 
     public static void main(String[] args) {
