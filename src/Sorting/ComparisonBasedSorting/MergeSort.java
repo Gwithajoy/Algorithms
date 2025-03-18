f43c1e5b509f3d0174164e76f7fe7d2da8b06e40
Gwithjoy <bangbabo131@gmail.com>
2025-03-18 22:07:11 +0900

    Study: 합병 정렬(Merge Sort) 알고리즘 구현 및 연습

--- src/Sorting/ComparisonBasedSorting/MergeSort.java
+++ src/Sorting/ComparisonBasedSorting/MergeSort.java
@@ -4,13 +4,37 @@ package Sorting.ComparisonBasedSorting;
 import java.util.Arrays;
 
 public class MergeSort {
-    
-    public static void mergeSort(int[] arr, int[] tmp, int left, int right) {
 
+    public static void mergeSort(int[] arr, int[] tmp, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, tmp, left, mid);
+            mergeSort(arr, tmp, mid + 1, right);
+            merge(arr, tmp, left, right, mid);
+        }
     }
 
     public static void merge(int[] arr, int[] tmp, int left, int right, int mid) {
+        int p = left;
+        int q = mid + 1;
+        int idx = p;
 
+        while (p <= mid || q <= right) {
+            if (p <= mid && q <= right) {
+                if (arr[p] <= arr[q]) {
+                    tmp[idx++] = arr[p++];
+                } else {
+                    tmp[idx++] = arr[q++];
+                }
+            } else if (p <= mid && q > right) {
+                tmp[idx++] = arr[p++];
+            } else {
+                tmp[idx++] = arr[q++];
+            }
+        }
+        for (int i = 0; i <= right; i++) {
+            arr[i] = tmp[i];
+        }
     }
 
     public static void main(String[] args) {
