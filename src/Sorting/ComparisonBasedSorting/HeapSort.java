2efb40a5887dec1281b455c877780d56e655052c
Gwithjoy <bangbabo131@gmail.com>
2025-03-19 13:43:24 +0900

    Study: 힙 정렬 알고리즘 구현

--- src/Sorting/ComparisonBasedSorting/HeapSort.java
+++ src/Sorting/ComparisonBasedSorting/HeapSort.java
@@ -5,11 +5,31 @@ import java.util.Arrays;
 public class HeapSort {
     
     public static void heapSort(int[] arr) {
+        for (int i = arr.length / 2 -1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
 
     }
 
     public static void heapify(int[] arr, int parentIdx, int size) {
+        int leftIdx = 2 * parentIdx + 1;
+        int rightIdx = 2 * parentIdx + 2;
+        int maxIdx = parentIdx;
 
+        if (leftIdx < size && arr[leftIdx] > arr[maxIdx]) {
+            maxIdx = leftIdx;
+        }
+        if (rightIdx < size && arr[rightIdx] > arr[maxIdx]) {
+            maxIdx = rightIdx;
+        }
+        if (parentIdx != maxIdx) {
+            swap(arr, parentIdx, maxIdx);
+            heapify(arr, maxIdx, size); // 이거 필수!
+        }
     }
 
     public static void swap(int[] arr, int i, int j) {
